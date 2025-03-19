1048ad94acdd0937f7c12c187a3a770a937bb110
jewel-106 <mjrcse106@gmail.com>
2025-03-19 11:43:31 +0600

    update accordingly cloudinary image hosting

--- src/app/material-component/mange-user/mange-user.component.ts
+++ src/app/material-component/mange-user/mange-user.component.ts
@@ -14,6 +14,7 @@ import { GlobalConstants } from 'src/app/shared/global-constants';
 export class MangeUserComponent implements OnInit {
 
   displayedColumns:string[] = ['name','email','contact_number','status'];
+  newData:any;
   dataSource:any;
   responseMessage:any;
 
