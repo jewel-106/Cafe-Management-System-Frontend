dd0bfa2290d58c1a358686f765ac53d85348ad94
jewel-106 <mjrcse106@gmail.com>
2025-03-19 12:50:49 +0600

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  //apiUrl: 'http://localhost:8080',
   //apiUrl: 'https://cafe-management-system-backend.vercel.app',
-  //apiUrl:'https://cafe-management-system-backend.vercel.app'
+  apiUrl:'https://cafe-management-system-backend-ztls.vercel.app'
 };
